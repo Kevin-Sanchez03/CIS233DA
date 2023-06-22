ad9c8fe219eb8e50ee10b86bbf84ed56374012a7
Kevin-Sanchez03 <124087055+Kevin-Sanchez03@users.noreply.github.com>
2023-06-22 08:58:49 -0700

    Coding 4b

--- Week 4/launchControl/assets/scripts/launch.js
+++ Week 4/launchControl/assets/scripts/launch.js
@@ -1,28 +1,29 @@
 let start = true;
-let alert;
+let msg;
 
 function tryLaunch() {
   if (brake == 1) {
     start = false;
-    alert = "Parking brake is on";
+    msg = "Parking brake is on";
   }
   if (tracControl == 1) {
     start = false;
-    alert = "Traction control is on";
+    msg = "Traction control is on";
   }
   if (battery == 0) {
     start = false;
-    alert = "battery is off";
+    msg = "battery is off";
   }
   if (shifter < 3) {
     start = false;
-    alert = "Not in sports mode";
+    msg = "Not in sports mode";
   }
   if (!start) {
-    alert = "Launch Control Failed";
+    document.getElementById("msg").innerText = msg;
     document.getElementById("launch").src = "./assets/images/lca0.png";
     document.getElementById("sputterAudio").play();
   } else {
+    document.getElementById("msg").innerText = "";
     document.getElementById("launch").src = "./assets/images/lca1.PNG";
     document.getElementById("launchAudio").play();
   }
